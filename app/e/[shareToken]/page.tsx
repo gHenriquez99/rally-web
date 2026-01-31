5ccb62f9067eeafbed4c40f4fe8f8752631d5599
Gus Henriquez <henriquez.gus99@gmail.com>
2026-01-31 14:49:52 -0500

    initial

---
+++ app/e/[shareToken]/page.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import { useEffect } from 'react'
+
+const APP_STORE_URL = 'https://apps.apple.com/app/rally/idAPP_STORE_ID'
+
+export default function ShareRedirect() {
+  useEffect(() => {
+    window.location.href = APP_STORE_URL
+  }, [])
+
+  return (
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      minHeight: '100vh',
+      fontFamily: 'system-ui, -apple-system, sans-serif',
+    }}>
+      <h1 style={{ fontSize: '24px', marginBottom: '16px' }}>
+        Redirecting to Rally...
+      </h1>
+      <p style={{ color: '#666' }}>
+        If you&apos;re not redirected automatically,{' '}
+        <a href={APP_STORE_URL} style={{ color: '#007AFF' }}>
+          tap here
+        </a>
+      </p>
+    </div>
+  )
+}
